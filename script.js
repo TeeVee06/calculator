5a8deb03ad3f9bcd7c21ff4d4c8ad44857661bd2
TeeVee06 <taylor@getfeedapp.com>
2021-12-29 13:53:15 -0500

    messing around

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function add() {
 function subtract() {
     c.value=parseInt(a.value)-parseInt(b.value);
 }
+function multiply() {
+    c.value=parseInt(a.value)*parseInt(b.value);
+}
+function divide () {
+    c.value=parseInt(a.value)/parseInt(b.value);
+}
